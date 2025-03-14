2ed807424c3c6394170088a76218e743e76d9c26
aaminter <aam.interpolyaris@gmail.com>
2025-03-14 19:59:37 +0300

    Code changed to message "dz3"

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,7 @@
 import java.util.Random;
 
-public class Program {
-    int age = new Random().nextInt(0, 37); // случайное значение от 0 до 36
-
-if(age >=18)
-
-    {
-        System.out.println("Да");
-    } else
-
-    {
-        System.out.println("Нет");
+public class Main {
+    public static void main(String[] args) {
+    System.out.println("dz3");
     }
-}   
+}
