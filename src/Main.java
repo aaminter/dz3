5c8b73fc5399a592445e5acb52791d7eafd99432
aaminter <aam.interpolyaris@gmail.com>
2025-03-14 19:50:50 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+
+public class Program {
+    int age = new Random().nextInt(0, 37); // случайное значение от 0 до 36
+
+if(age >=18)
+
+    {
+        System.out.println("Да");
+    } else
+
+    {
+        System.out.println("Нет");
+    }
+}   
